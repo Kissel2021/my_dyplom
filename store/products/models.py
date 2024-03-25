e29e6aeb12e9bf7ac3493c30aae1275248d6bccc
vlad <kisselev2020@gmail.com>
2024-03-25 21:54:28 +0300

    Creating models, MTV, dinamic urls, add base templates

--- store/products/models.py
+++ store/products/models.py
@@ -2,18 +2,28 @@
 
 from django.db import models
 
+
 class ProductCategory(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField()
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
     image = models.CharField(max_length=255)
+    #image = models.ImageField(upload_to='products_images') У ребят так
     description = models.TextField()
     short_description = models.CharField(max_length=64)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    quantity = models.IntegerField()
+    quantity = models.PositiveIntegerField()
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Продукт: {self.name} | Категория: {self.category.name}'
+
 
 class User(models.Model):
     username = models.CharField(max_length=45)
@@ -22,6 +32,7 @@ class User(models.Model):
     email = models.EmailField()
     password = models.CharField(max_length=45)
     image = models.CharField(max_length=255)
+
 
 class Basket(models.Model):
     quantity = models.IntegerField()
